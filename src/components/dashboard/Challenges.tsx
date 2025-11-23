0f728c34d8ab931c4d6fbc2632b8d1f0b43bbc76
3roland59 <amarteyroland359@gmail.com>
2025-11-23 15:57:22 +0000

    fix(deploy issue): fix minor issue

--- src/components/dashboard/Challenges.tsx
+++ src/components/dashboard/Challenges.tsx
@@ -41,7 +41,7 @@ const Challenges = () => {
     );
   }
 
-  if (challenges?.length == 0 || challenges) {
+  if (challenges?.length == 0 || !challenges) {
     return (
       <div className="max-w-4xl mx-auto p-4 sm:p-6">
         {/* Header */}
